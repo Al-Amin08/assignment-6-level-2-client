5f61dedfc30658a0129caf602d9ffa8072539599
Al-Amin08 <mehadislam08@gmail.com>
2025-12-17 18:07:21 +0600

    add dashboard layout

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,7 +1,5 @@
-import * as React from "react"
+import * as React from "react";
 
-import { SearchForm } from "@/components/search-form"
-import { VersionSwitcher } from "@/components/version-switcher"
 import {
   Sidebar,
   SidebarContent,
@@ -13,149 +11,28 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarRail,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
+import Logo from "@/assets/icons/Logo";
+import { Link } from "react-router";
+import { adminSidebarItems } from "@/routes/adminSidebarItems";
 
 // This is sample data.
 const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Installation",
-          url: "#",
-        },
-        {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Building Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
-        },
-        {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Rendering",
-          url: "#",
-        },
-        {
-          title: "Caching",
-          url: "#",
-        },
-        {
-          title: "Styling",
-          url: "#",
-        },
-        {
-          title: "Optimizing",
-          url: "#",
-        },
-        {
-          title: "Configuring",
-          url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "API Reference",
-      url: "#",
-      items: [
-        {
-          title: "Components",
-          url: "#",
-        },
-        {
-          title: "File Conventions",
-          url: "#",
-        },
-        {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
-        },
-      ],
-    },
-  ],
-}
+  navMain: adminSidebarItems,
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
       <SidebarHeader>
-        <VersionSwitcher
-          versions={data.versions}
-          defaultVersion={data.versions[0]}
-        />
-        <SearchForm />
+        <div className="flex">
+          <Logo />
+
+          <h3 className="my-auto text-2xl font-semibold ">
+            Digital
+            <span className="text-sky-600 font-bold ">Wallet</span>
+          </h3>
+        </div>
       </SidebarHeader>
       <SidebarContent>
         {/* We create a SidebarGroup for each parent. */}
@@ -167,7 +44,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
                     <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
+                      <Link to={item.url}>{item.title}</Link>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
@@ -178,5 +55,5 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
---
+++ src/routes/adminSidebarItems.tsx
@@ -0,0 +1,65 @@
+export const adminSidebarItems = [
+  {
+    title: "Admin Dashboard",
+    url: "#",
+    items: [
+      {
+        title: "List of All Users",
+        url: "#",
+      },
+
+      {
+        title: "My Profile",
+        url: "#",
+      },
+    ],
+  },
+
+  {
+    title: "Commission Management",
+    url: "#",
+    items: [
+      {
+        title: "Commission History",
+        url: "#",
+      },
+    ],
+  },
+
+  {
+    title: "Transaction Management",
+    url: "#",
+    items: [
+      {
+        title: "All Transaction History",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Wallet Management",
+    url: "#",
+    items: [
+      {
+        title: "All Wallets Info",
+        url: "#",
+      },
+      {
+        title: "Unblock User",
+        url: "#",
+      },
+      {
+        title: "Block User",
+        url: "#",
+      },
+      {
+        title: "Approve Agent",
+        url: "#",
+      },
+      {
+        title: "Suspend Agent",
+        url: "#",
+      },
+    ],
+  },
+];
